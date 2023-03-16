87e83a333281996e491777c99c7cf4910b2beacf
mohamedis8890 <mohamed.is8890@gmail.com>
2023-03-16 23:51:30 +0000

    Task 0-5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - the main function, prints a string.
+ *
+ * Return: 0, As required
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
