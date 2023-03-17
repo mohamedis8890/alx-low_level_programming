63f4d49dd9112acb3e6c7fbbc1dd0e5f97d65378
mohamedis8890 <mohamed.is8890@gmail.com>
2023-03-17 19:08:42 +0000

    Task 0-4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - the main function, prints the alphabet
+*
+* Return: 0, as required
+*/
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if(c != 'q' && c != 'e')
+			putchar(c);
+		c = c + 1;
+	}
+	putchar('\n');
+	return (0);
+}
