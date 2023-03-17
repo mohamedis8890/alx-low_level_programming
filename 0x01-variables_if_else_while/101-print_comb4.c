7baaa6ca0b772927d90d867b31f1b1e2c1b90abc
mohamedis8890 <mohamed.is8890@gmail.com>
2023-03-17 22:54:02 +0000

    Task 101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+* main - the main function, prints two-digit combinations
+*
+* Return: 0, as required
+*/
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 48; i < 56; i++)
+	{
+		for (j = i + 1; j < 57; j++)
+		{
+			for (k = j + 1; k < 58; k++)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(k);
+				if (i < 55)
+				{
+					putchar(44);
+					putchar(32);
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
