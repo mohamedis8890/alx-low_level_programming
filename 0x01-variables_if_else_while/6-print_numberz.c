6aa6ebf9b405080414145e03be893cda14c426f2
mohamedis8890 <mohamed.is8890@gmail.com>
2023-03-17 20:47:18 +0000

    Task 0-6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - the main function, prints numberz
+*
+* Return: 0, as required
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+		putchar(n);
+	putchar('\n');
+	return (0);
+}
