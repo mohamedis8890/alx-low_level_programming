fce93e610abae8b2322e5e391644bd859186dcc6
mohamedis8890 <mohamed.is8890@gmail.com>
2023-03-22 02:21:20 +0000

    Fix: betty documentation style

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,10 +1,11 @@
 #include "main.h"
+
 /**
-* _isalpha: checks if a letter is alphabetic character
+* _isalpha - checks if a letter is alphabetic character
 * @c: character to check
 *
 * Description: This function accepts a char and checks it aginst ascii table
-* representations of lower and upper case alphabet
+* representations of lower and upper case alphabet.
 * Return: 1 if c is alphabetic, 0 otherwise
 */
 int _isalpha(int c)
