d426580ecd0b9ee44d37da8999f2ea538e41b783
mohamedis8890 <mohamed.is8890@gmail.com>
2023-03-24 22:25:01 +0000

    Task 4-3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* print_numbers - prints numbers 0 to 9 follwed ny a nwe line
+*
+* Return: null
+*/
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+		_putchar;
+
+	_putchar('\n');
+
+	return;
+}
