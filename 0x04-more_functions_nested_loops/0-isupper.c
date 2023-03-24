0e40ea41673d00513f5ee1be2f5c14d6c79e26ef
mohamedis8890 <mohamed.is8890@gmail.com>
2023-03-24 22:00:08 +0000

    Betty style

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,7 +2,7 @@
 * _isupper - checks if a character is uppercase or lowercase
 *
 * @c: the character to be checked
-* Returns: 1 if c is uppercase or 0 otherwise
+* Return: 1 if c is uppercase or 0 otherwise
 */
 
 int _isupper(int c)
