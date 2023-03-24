b1e3361e7abd2df4d0428ecb3677d2d557c26445
mohamedis8890 <mohamed.is8890@gmail.com>
2023-03-24 23:10:30 +0000

    Task 4-4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* print_most_numbers - prints numbers except 2 and 4
+*
+* Return: 0, as required
+*/
+void print_most_numbers(void)
+{
+	int x;
+
+	for (x = 48; x <= 57; x++)
+	{
+		if (x != 50 && x != 52)
+			_putchar(x);
+
+	}
+}
