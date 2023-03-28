b341f9b4f3c70a77d2b5a557051d8134f7f2bba4
mohamedis8890 <mohamed.is8890@gmail.com>
2023-03-28 01:18:11 +0000

    Task 5-3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _puts - prints a string, followed by a new line, to stdout.
+ *
+ * @str: the string to print.
+ * Return: void.
+ */
+void _puts(char *str)
+{
+	int count;
+
+	for (count = 0; str[count] != '\0'; count++)
+		_putchar(str[count]);
+}
