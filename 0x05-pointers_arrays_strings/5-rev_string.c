4be4618e7f1cb7079f09d56a5a783a7e2f667750
mohamedis8890 <mohamed.is8890@gmail.com>
2023-03-28 02:43:20 +0000

    Task 5-4

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+/**
+ * rev_string - reverses a string.
+ *
+ * @s: string to be reversed
+ * Return: void.
+ */
+void rev_string(char *s)
+{
+        int a = 0, b, c;
+        char d;
+
+        while (s[a] != '\0')
+        {
+                a++;
+        }
+        c = a - 1;
+        for (b = 0; c >= 0 && b < c; c--, b++)
+        {
+                d = s[b];
+                s[b] = s[c];
+                s[c] = d;
+        }
+}
