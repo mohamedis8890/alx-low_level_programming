dad6f922a1bb83fda347aca653cc64c14ee9f705
mohamedis8890 <mohamed.is8890@gmail.com>
2023-03-31 11:59:59 +0000

    Correction

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,14 +9,14 @@
  */
 char *cap_string(char *s)
 {
-	char seps[] = "\t\n ,;:.!?\"(){}";
+	char seps[] = "\t\n ,;.!?\"(){}";
 
 	int j;
 	int i;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; j < 15; j++)
+		for (j = 0; j < 14; j++)
 		{
 			if (seps[j] == s[i] && s[i + 1] != 32)
 				if (s[i + 1] >= 97 && s[i + 1] <= 122)
