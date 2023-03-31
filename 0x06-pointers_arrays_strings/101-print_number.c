8ebe85912b05e01613e648d019383a204d7b4326
mohamedis8890 <mohamed.is8890@gmail.com>
2023-03-31 17:16:33 +0000

    Task 102

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_number - prints an integer to the stdout.
+ *
+ * @n: the integer to  be printed
+ *
+ * Return: void
+ */
+void print_number(int n)
+{
+	char c;
+
+	c = n + '0';
+
+	_putchar(c);
+}
