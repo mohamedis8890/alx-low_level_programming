9ff1f8875551bb49ca0342444e7f269db9037864
mohamedis8890 <mohamed.is8890@gmail.com>
2023-04-03 22:05:59 +0000

    Task 7-2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strchr - Write a function that
+ *
+ * @s: athe string to search in
+ *
+ * @c: the character to be searched for
+ *
+ * Return: a pointer to to the first occurence of c or NULL
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		if (*(s + i) == c)
+			return (s + i);
+
+	return ('\0');
+}
