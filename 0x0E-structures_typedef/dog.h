233dd7e6cd8be6407899e19f6dfd0fe4f743649f
mohamedis8890 <mohamed.is8890@gmail.com>
2023-04-17 10:51:45 +0000

    Betty style

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,17 @@
-struct dog {
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * struct dog - a representation of a dog
+ * @name: dog's name
+ * @owner: dog's owner
+ * @age: dog's age
+ *
+ * Description: Longer description
+ */
+struct dog
+{
 	char *name;
 	char *owner;
 	float age;
 };
+#endif
