549805a85093201da4b114375c3be2e8030081b7
mohamedis8890 <mohamed.is8890@gmail.com>
2023-09-28 21:18:54 +0000

    8-0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,18 +1,20 @@
 #include "main.h"
 /**
  * _puts_recursion - prints a string, followed by a new line.
- * @s: the string to print
+ * @s: the string to print.
  *
- * Return: void
+ * Return: void.
  */
 void _puts_recursion(char *s)
 {
-	if (*(s) == '\0')
+	char c = *s;
+
+	if (c == '\0')
 	{
 		_putchar('\n');
 		return;
 	}
 
-	_putchar(*(s));
+	_putchar(c);
 	_puts_recursion((s + 1));
 }
--- 0x0D-preprocessor/1-pi.h
+++
@@ -1,9 +0,0 @@
-#ifndef PI_H
-#define PI_H
-
-/**
-* PI - abbreviation for the token 3.14159265359.
-*/
-#define PI 3.14159265359
-
-#endif
