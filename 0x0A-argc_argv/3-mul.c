5b63c99c174bc7d8b6b316879744530bd1dc9004
mohamedis8890 <mohamed.is8890@gmail.com>
2023-10-07 13:24:18 +0000

    A-3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * main - multiplies two numbers.
+ * @argc: arguments vector size
+ * @argv: arguments vector
+ *
+ * Return: always zero.
+ */
+int main(int argc, char *argv[])
+{
+	int first, second;
+
+	if (argc < 3)
+	{
+		printf("Error.\n");
+		return (1);
+	}
+
+	first = atoi(argv[1]);
+	second = atoi(argv[2]);
+
+	printf("%d\n", first * second);
+
+	return (0);
+}
