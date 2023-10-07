f88372a46c667a9333d36d6faca8d1e0525fcc72
mohamedis8890 <mohamed.is8890@gmail.com>
2023-10-07 13:08:25 +0000

    A-0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - prints the program's name, followed by a new line.
+ * @argc: arguments vector size
+ * @argv: arguments vector
+ *
+ * Return: always zero.
+ */
+int main(int argc, char **argv)
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
