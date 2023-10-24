d282700218680ce43a87dd2a125944c631ead8e5
mohamedis8890 <mohamed.is8890@gmail.com>
2023-10-24 23:16:14 +0300

    13-0_betty

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,16 +8,15 @@
  */
 size_t print_listint(const listint_t *h)
 {
-		size_t count;
+	size_t count;
 
-		count = 0;
-		while (h != NULL)
-		{
-				printf("%d\n", h->n);
-				h = h->next;
-				count++;
-		}
+	count = 0;
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
 
-		return (count);
+	return (count);
 }
-
