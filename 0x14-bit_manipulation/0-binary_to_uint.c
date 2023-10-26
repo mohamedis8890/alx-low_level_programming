2330b2d3a93b83774cef07d0e9b596b91b6ba58c
mohamedis8890 <mohamed.is8890@gmail.com>
2023-10-27 01:33:24 +0200

    14-0_1

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,13 +9,13 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int n = 0;
 
+	if (b == NULL)
+	{
+		return (0);
+	}
+
 	while (*b != '\0')
 	{
-		if (b == NULL)
-		{
-			return (0);
-		}
-
 		if (*b != '1' && *b != '0')
 			return (0);
 
